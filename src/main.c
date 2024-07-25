4c898da9d743fbc9cfb99dd7e2b7760c3fe445bb
seyednasermoravej <seyednasermoravej@gmail.com>
2024-07-25 06:20:44 +0330

    added i2c scannder function

--- src/main.c
+++ src/main.c
@@ -4,23 +4,9 @@
 
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
-// #define DT_SPEC_AND_COMMA(node_id, prop, idx) \
-// 	GPIO_DT_SPEC_GET_BY_IDX(node_id, prop, idx),
-
-
-// static const struct gpio_dt_spec allAddress[] = {
-//     DT_FOREACH_PROP_ELEM(DT_NODELABEL(address), gpios, DT_SPEC_AND_COMMA)
-// };
-#define LED2_NODE DT_ALIAS(led2)
-// static const struct gpio_dt_spec led00 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
-
-
-
-
-// static const struct gpio_dt_spec address = GPIO_DT_SPEC_GET_OR(ADDR_NODE, gpios,
-// 							      {0});
 
 int threadsCreation();
+void i2cScanner();
 
 extern void puzzleThreadCreate();
 extern void mqttThreadCreate();
@@ -35,39 +21,6 @@ int main()
     //http request for getting DFU
     sem_wait(&dhcpActive);
     sem_destroy(&dhcpActive);
-
-
-    // const struct device *i2c_dev;
-    // uint8_t addr;
-    // int ret;
-
-//     i2c_dev = device_get_binding("I2C_0");
-//     if (!i2c_dev) {
-//         printf("Error: I2C device not found\n");
-//         return;
-//     }
-
-//     printf("Scanning I2C bus...\n");
-// while (1)
-// {
-
-
-//     for (addr = 1; addr < 128; addr++) {
-//         struct i2c_msg msg;
-//         uint8_t dummy_data = 0;
-
-//         msg.buf = &dummy_data;
-//         msg.len = 1;
-//         msg.flags = I2C_MSG_WRITE | I2C_MSG_STOP;
-
-//         ret = i2c_transfer(i2c_dev, &msg, 1, addr);
-//         if (ret == 0) {
-//             printf("Device found at address 0x%02X\n", addr);
-//         }
-//     }
-//     k_msleep(1000);
-// }
-
 
     threadsCreation();
     struct MqttMsg *send = (struct MqttMsg *)k_malloc(sizeof(struct MqttMsg));
@@ -90,4 +43,39 @@ int threadsCreation()
 {
     mqttThreadCreate();
     puzzleThreadCreate();
+}
+
+void i2cScanner()
+{
+
+    const struct device *i2c_dev;
+    uint8_t addr;
+    int ret;
+
+    i2c_dev = device_get_binding("I2C_0");
+    if (!i2c_dev) {
+        printf("Error: I2C device not found\n");
+        return;
+    }
+
+    printf("Scanning I2C bus...\n");
+    while (1)
+    {
+
+
+        for (addr = 1; addr < 128; addr++) {
+            struct i2c_msg msg;
+            uint8_t dummy_data = 0;
+
+            msg.buf = &dummy_data;
+            msg.len = 1;
+            msg.flags = I2C_MSG_WRITE | I2C_MSG_STOP;
+
+            ret = i2c_transfer(i2c_dev, &msg, 1, addr);
+            if (ret == 0) {
+                printf("Device found at address 0x%02X\n", addr);
+            }
+        }
+        k_msleep(1000);
+    }
 }
