824140e94e4193daed251a2a65c09ad5d65203ee
seyednasermoravej <seyednasermoravej@gmail.com>
2024-12-23 18:31:30 +0330

    shifting to 4 digits display

--- src/doorKeypad.cpp
+++ src/doorKeypad.cpp
@@ -35,25 +35,25 @@ void DoorKeypad:: buttonsHandler(struct input_event *val)
 
 }
 
-// static const uint8_t segment_map[128] = {
-//     0xC0, // 0b11000000 ['0'] 
-//     0xF9, // 0b11111001 ['1'] 
-//     0xA4, // 0b10100100 ['2'] 
-//     0xB0, // 0b10110000 ['3'] 
-//     0x99, // 0b10011001 ['4'] 
-//     0x92, // 0b10010010 ['5'] 
-//     0x82, // 0b10000010 ['6'] 
-//     0xF8, // 0b11111000 ['7'] 
-//     0x80, // 0b10000000 ['8'] 
-//     0x90, // 0b10010000 ['9'] 
-//     0x88, // 0b10001000 ['A'] 
-//     0x83, // 0b10000011 ['b'] 
-//     0xC6, // 0b11000110 ['C'] 
-//     0xA1, // 0b10100001 ['d'] 
-//     0x86, // 0b10000110 ['E'] 
-//     0x8E, // 0b10001110 ['F'] 
-//     0xFF  // Blank (all off) [' '] 
-// };
+static const uint8_t digit_segments[128] = {
+    0xC0, // 0b11000000 ['0'] 
+    0xF9, // 0b11111001 ['1'] 
+    0xA4, // 0b10100100 ['2'] 
+    0xB0, // 0b10110000 ['3'] 
+    0x99, // 0b10011001 ['4'] 
+    0x92, // 0b10010010 ['5'] 
+    0x82, // 0b10000010 ['6'] 
+    0xF8, // 0b11111000 ['7'] 
+    0x80, // 0b10000000 ['8'] 
+    0x90, // 0b10010000 ['9'] 
+    0x88, // 0b10001000 ['A'] 
+    0x83, // 0b10000011 ['b'] 
+    0xC6, // 0b11000110 ['C'] 
+    0xA1, // 0b10100001 ['d'] 
+    0x86, // 0b10000110 ['E'] 
+    0x8E, // 0b10001110 ['F'] 
+    0xFF  // Blank (all off) [' '] 
+};
 static const uint8_t segment_map[128] = {
     // Control characters (0–31)
     0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // 0x00–0x07
@@ -192,7 +192,47 @@ void DoorKeypad:: messageHandler(struct MqttMsg *msg)
         char field[] = "display";
         if((strcmp(field, command) == 0))
         {
-            // display->print(msg->msg);
+            	while(1)
+	    {
+        for(uint8_t i = 0; i < 8; i++)
+        {
+        gpio_pin_set_dt(&display[8 + i], (digit_segments[1]>> i) &0x01);
+        }
+        LOG_INF("Entered while loop");
+        gpio_pin_set_dt(&display[0], 0);
+        gpio_pin_set_dt(&display[1], 0);
+        gpio_pin_set_dt(&display[2], 0);
+        gpio_pin_set_dt(&display[3], 0);
+        gpio_pin_set_dt(&display[4], 1);
+        gpio_pin_set_dt(&display[5], 1);
+        gpio_pin_set_dt(&display[6], 1);
+        gpio_pin_set_dt(&display[7], 1);
+        // gpio_pin_set_dt(&display[9], 1);
+        // gpio_pin_set_dt(&display[10], 1);
+        // gpio_pin_set_dt(&display[11], 1);
+        // gpio_pin_set_dt(&display[12], 1);
+        // gpio_pin_set_dt(&display[13], 1);
+        // gpio_pin_set_dt(&display[14], 1);
+        // gpio_pin_set_dt(&display[15], 1);
+        k_msleep(100);
+        // gpio_pin_set_dt(&display[0], 0);
+        // gpio_pin_set_dt(&display[1], 0);
+        // gpio_pin_set_dt(&display[2], 0);
+        // gpio_pin_set_dt(&display[3], 0);
+        // gpio_pin_set_dt(&display[4], 0);
+        // gpio_pin_set_dt(&display[5], 0);
+        // gpio_pin_set_dt(&display[6], 0);
+        // gpio_pin_set_dt(&display[7], 0);
+        // gpio_pin_set_dt(&display[8], 0);
+        // gpio_pin_set_dt(&display[9], 0);
+        // gpio_pin_set_dt(&display[10], 0);
+        // gpio_pin_set_dt(&display[11], 0);
+        // gpio_pin_set_dt(&display[12], 0);
+        // gpio_pin_set_dt(&display[13], 0);
+        // gpio_pin_set_dt(&display[14], 0);
+        // gpio_pin_set_dt(&display[15], 0);
+        // k_usleep(1);
+    }
         }
         else
         {
