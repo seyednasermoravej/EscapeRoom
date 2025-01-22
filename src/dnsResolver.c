2913183d55950c802a4432ef7bcf0cef3bbfb8c4
seyednasermoravej <seyednasermoravej@gmail.com>
2025-01-23 00:43:40 +0330

    Bram wifi config added to the dhcp clinet

--- src/dnsResolver.c
+++ src/dnsResolver.c
@@ -396,10 +396,14 @@ int dnsResolver(const char *deviceName, char *queryName, char *serverIpAddress)
 #if defined(CONFIG_BOARD_RPI_PICO_RP2040_W)
 		struct wifi_connect_req_params connect_params = {
 #ifdef NASER
-        .ssid = "Safineh-2",
-        .ssid_length = strlen("Safineh-2"),
-        .psk = "gXvNavG4mg@9c9Z8",
-        .psk_length = strlen("gXvNavG4mg@9c9Z8"),
+        // .ssid = "Naser",
+        // .ssid_length = strlen("Naser"),
+        // .psk = "nasimore",
+        // .psk_length = strlen("nasimore"),
+        .ssid = "SAFINE-3-2.4G",
+        .ssid_length = strlen("SAFINE-3-2.4G"),
+        .psk = "EYE7GLQB73",
+        .psk_length = strlen("EYE7GLQB73"),
 #elif defined(BRAM)
         .ssid = "D21CONTROL",
         .ssid_length = strlen("D21CONTROL"),
