ebb5f68325b8f75fb32bf23cdbc64cf5f9d9f291
seyednasermoravej <seyednasermoravej@gmail.com>
2025-01-31 14:41:09 +0330

    configuration changed to bram to test the fridge

--- inc/main.h
+++ inc/main.h
@@ -25,10 +25,10 @@
 #include <zephyr/sys/printk.h>
 #include <stdbool.h>
 #include <zephyr/sys/sys_heap.h>
-#define NASER 
+#define BRAM 
 
 #define PUZZLE_DISPLAY_LEN 8
-#define FIRMWARE_VERSION    "1.2" 
+#define FIRMWARE_VERSION    "1.3" 
 // #define WATCH_DOG
 /*
  * To use this sample the devicetree's /aliases must have a 'watchdog0' property.
